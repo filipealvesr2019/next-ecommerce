b3bb223fb95f51c9c08929e8649f77ded16b3b7e
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2024-09-27 17:04:17 -0300

    feat: migrando pro nextjs... dia 3

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,4 @@
-"use client"
+
 import styles from "./page.module.css";
 import Navbar from "@/components/Navbar/Navbar";
 import Slider from "@/components/Slider/Slider";
@@ -8,20 +8,14 @@ import CategoryCarousel from "@/components/CategoryCarousel/CategoryCarousel";
 import Header from "@/components/Header/Header";
 
 
-// import Header from "./Header";
-// import Categories from "./Categories";
-// import CategoryCarousel from "./CategoryCarousel";
-// import DiscountImageCarousel from "./DiscountImageCarousel"; // Remover o espaço extra no final
-// import NewArrivals from "./NewArrivals";
-// import Footer from "./Footer";
-// export const metadata = {
-//   title: "Home - Loja Mediewal", // Define o título da página
-//   description:
-//     "Veja as últimas novidades em nossa loja, com uma seleção de produtos novos.", // Descrição da página
-//   icons: {
-//     icon: "/favicon.svg", // Define o favicon
-//   },
-// };
+export const metadata = {
+  title: "Home - Loja Mediewal", // Define o título da página
+  description:
+    "Veja as últimas novidades em nossa loja, com uma seleção de produtos novos.", // Descrição da página
+  icons: {
+    icon: "/favicon.svg", // Define o favicon
+  },
+};
 
 export default function Home() {
  
