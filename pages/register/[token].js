ff3da0688ad3163c5cecc143d5982aac843a9ee8
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2024-09-28 16:08:30 -0300

    feat: migrando pro nextjs... dia 4

--- pages/register/[token].js
+++ pages/register/[token].js
@@ -1,97 +1,103 @@
 "use client"
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-import styles from "./RegisterLink.module.css";
+
+import style from "./ResetPasswordPage.module.css";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { useConfig } from "../../context/ConfigContext";
-import Header from "@/components/Header/Header";
-import Navbar from "@/components/Navbar/Navbar";
-import RootLayout from "@/app/layout";
+import Header from '@/components/Header/Header';
+import { useConfig } from '../../../../context/ConfigContext';
 
+const ResetPasswordPage = ({params}) => {
+  const { token } = params; // Extrai o token da URL
 
-
-const RegisterLink = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
+  const [newPassword, setNewPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [message, setMessage] = useState('');
   const { apiUrl } = useConfig();
- 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const newUser = { email, password, role: "customer" };
-      await axios.post(`${apiUrl}/register/request`, newUser);
-      
-      // Limpar o formulário após o envio bem-sucedido
-      setEmail("");
-      setPassword("");
-      setError("");
-      
-      // Exibir mensagem de sucesso
-      toast.success("Email enviado com sucesso! Verifique sua caixa de email.");
-      
-    } catch (err) {
-      if (err.response && err.response.status === 400) {
-        // Email já cadastrado
-        toast.error("Este email já está cadastrado. Por favor, use outro email.");
-      } else {
-        // Outro erro
-        toast.error("Ocorreu um erro ao enviar o email. Tente novamente.");
+  useEffect(() => {
+    // Verifica se o token está presente na URL
+    if (!token) {
+      setMessage('Token inválido.');
+    }
+  }, [token]);
+
+  const handleResetPassword = async () => {
+    try {alert
+      // Verificar se as senhas coincidem
+      if (newPassword !== confirmPassword) {
+        setMessage('As senhas não coincidem.');
+        return;
       }
-      setError(err.response?.data.message || "Ocorreu um erro.");
+
+      // Enviar a solicitação para redefinir a senha
+      const response = await axios.post(`${apiUrl}/reset-password/:token`, {
+        token,
+        newPassword,
+        confirmPassword,
+      });
+
+      // Exibir a mensagem de sucesso
+      setMessage(response.data.error);
+      toast.success("Senha atualisada com sucesso!");
+
+    } catch (error) {
+      // Exibir mensagens de erro
+      setMessage(error.response.data.message);
     }
   };
 
   return (
-    <>
-        <RootLayout>
-
-
+    <div>
       <Header />
-      <Navbar />
       {/* <Helmet>
-        <title>Cadastro de usuário  - Loja Mediewal</title>
+        <title>Mudar Senha - Loja Mediewal</title>
         <meta name="description" content="Veja as últimas novidades em nossa loja, com uma seleção de produtos novos." />
       </Helmet> */}
-      <div className={styles.formContainer}>
-        <h1 className={styles.formContainer__h1}>Digite seu email</h1>
-
-        
-        <form onSubmit={handleSubmit} className={styles.formContainer__form}>
-          <ToastContainer
-            position="top-center"
-            autoClose={5000}
-            hideProgressBar={false}
-            newestOnTop={false}
-            closeOnClick
-            rtl={false}
-            pauseOnFocusLoss
-            draggable
-            pauseOnHover
-            theme="light"
-            style={{ marginTop: "8rem" }}
-          />
-          <label className={styles.formContainer__label}>Email:</label>
-          <div className={styles.formContainer__div}>
-            <input
-              type="email"
-              required
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className={styles.formContainer__input}
-              placeholder="email@exemplo.com"
-            />
-            <button type="submit" className={styles.formContainer__button}>Enviar</button>
-          </div>
-        </form>
+      <div className={style.formContainer}>
+      <ToastContainer
+          position="top-center"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+          style={{ marginTop: "8rem" }}
+        />
+      <h2  className={style.formContainer__h2}>Redefinir Senha</h2>
+      <div className={style.flex}>
+        <span  className={style.formContainer__span}>      {message  && <p>{message}</p>}
+        </span>
+        <label           className={style.formContainer__label}
+        >Nova Senha:</label>
+        <input
+          type="password"
+          value={newPassword}
+          onChange={(e) => setNewPassword(e.target.value)}
+          className={style.formContainer__input}
+        />
       </div>
+      <div className={style.flex}>
+        <label  className={style.formContainer__label}>Confirmar Senha:</label>
+        <input
+          type="password"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+          className={style.formContainer__input}
 
-        </RootLayout>
+        />
+      </div>
+      <button onClick={handleResetPassword}           className={style.formContainer__button}
+      >Redefinir Senha</button>
 
-    </>
+      </div>
+    </div>
   );
 };
 
-export default RegisterLink;
+export default ResetPasswordPage;
