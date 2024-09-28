9a2b15e2c993b3850ed225c7b9e27df1b8141afd
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2024-09-28 17:01:03 -0300

    feat: migrando pro nextjs... dia 4

--- pages/register-user/[token].js
+++ pages/register-user/[token].js
@@ -1,11 +1,18 @@
-"use client";
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-import style from "./ResetPasswordPage.module.css";
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
+import CheckIcon from '@mui/icons-material/Check';
+import CloseIcon from '@mui/icons-material/Close';
+import styles from './Register.module.css';
+
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+
 import Header from '@/components/Header/Header';
+import Navbar from '@/components/Navbar/Navbar';
+import { useConfig } from '../../context/ConfigContext';
 export const getServerSideProps = async (context) => {
   const { token } = context.query;
   
@@ -16,54 +23,76 @@ export const getServerSideProps = async (context) => {
   };
 };
 
-const ResetPasswordPage = ({ token }) => {
 
-  const [newPassword, setNewPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
+function RegisterUser({token}) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const [message, setMessage] = useState('');
-console.log(token)
+  const [error, setError] = useState('');
+
+  const [showPassword, setShowPassword] = useState(null);
+  const [containsSpecialCharacter, setContainsSpecialCharacter] = useState(false);
+  const [containsNumber, setContainsNumber] = useState(false);
+  const [containsCapitalLetter, setContainsCapitalLetter] = useState(false);
+  const [containsLowerCase, setContainsLowerCase] = useState(false);
+  const [isAddButtonDisabled, setIsAddButtonDisabled] = useState(true);
+  const { apiUrl } = useConfig();
+
   useEffect(() => {
-    // Verifies if the token is present in the URL
-    if (!token) {
-      setMessage('Token inválido.');
-    }
+    // O token agora está disponível aqui, você pode usá-lo como desejar
+
   }, [token]);
 
-  const handleResetPassword = async () => {
-    if (!token) {
-      setMessage('Token não encontrado.');
-      return;
+  useEffect(() => {
+    // Verifica se todos os critérios estão atendidos
+    if (containsSpecialCharacter && containsNumber && containsCapitalLetter && containsLowerCase) {
+      setIsAddButtonDisabled(false);
+    } else {
+      setIsAddButtonDisabled(true);
     }
+  }, [containsSpecialCharacter, containsNumber, containsCapitalLetter, containsLowerCase]);
 
-    try {
-      // Verify if passwords match
-      if (newPassword !== confirmPassword) {
-        setMessage('As senhas não coincidem.');
-        return;
-      }
+  const handlePasswordChange = (e) => {
+    const newPassword = e.target.value;
+    setPassword(newPassword);
 
-      // Send request to reset password
-      const response = await axios.post(`https://serveradmin-whhj.onrender.com/reset-password/${token}`, {
-        token, // use token from the URL
-        newPassword,
-        confirmPassword,
-      });
+    // Verifica se há algum caractere especial na senha
+    const specialCharacterPattern = /[\\;?-^.!'{:@#$%^&"_(¨¨||/+,.=)_£0}*|<>`]/;
+    setContainsSpecialCharacter(specialCharacterPattern.test(newPassword));
 
-      // Display success message
-      setMessage(response.data.error ? response.data.error : 'Senha atualizada com sucesso!');
-      toast.success("Senha atualizada com sucesso!");
+    const OneNumber = /[0-9]/;
+    setContainsNumber(OneNumber.test(newPassword));
 
-    } catch (error) {
-      // Display error messages
-      setMessage(error.response?.data?.message || 'Ocorreu um erro ao redefinir a senha.');
-    }
+    const OneCapitalLetter = /[A-Z]/;
+    setContainsCapitalLetter(OneCapitalLetter.test(newPassword));
+
+    const OneLowerCase = /[a-z]/;
+    setContainsLowerCase(OneLowerCase.test(newPassword));
   };
 
+  const role = 'customer'; // Definindo o papel (role) como 'customer' por padrão
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(`${apiUrl}/register/${token}`, { email, password, role });
+      setMessage(response.data.message);
+      toast.success("Usuário registrado com sucesso.");
+
+    } catch (error) {
+      setError(error.response.data.error);
+    }
+  };
+ 
   return (
-    <div>
+    <>
       <Header />
-      <div className={style.formContainer}>
-        <ToastContainer
+      <Navbar />
+      {/* <Helmet>
+        <title>Cadastro de usuário  - Loja Mediewal</title>
+        <meta name="description" content="Veja as últimas novidades em nossa loja, com uma seleção de produtos novos." />
+      </Helmet> */}
+      <ToastContainer
           position="top-center"
           autoClose={5000}
           hideProgressBar={false}
@@ -76,35 +105,47 @@ console.log(token)
           theme="light"
           style={{ marginTop: "8rem" }}
         />
-        <h2 className={style.formContainer__h2}>Redefinir Senha</h2>
-        <div className={style.flex}>
-          {message && <p>{message}</p>}
-          <label className={style.formContainer__label}>Nova Senha:</label>
-          <input
-            type="password"
-            value={newPassword}
-            onChange={(e) => setNewPassword(e.target.value)}
-            className={style.formContainer__input}
-          />
-        </div>
-        <div className={style.flex}>
-          <label className={style.formContainer__label}>Confirmar Senha:</label>
-          <input
-            type="password"
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-            className={style.formContainer__input}
-          />
-        </div>
-        <button
-          onClick={handleResetPassword}
-          className={style.formContainer__button}
-        >
-          Redefinir Senha
-        </button>
+      <div className={styles.container}>
+        <form onSubmit={handleSubmit} className={styles.formContainer}>
+          <h2 className={styles.h2}>Cadastro</h2>
+          <div className={styles.specs}>
+            <label className={styles.label}>Email:</label>
+            <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required className={styles.input} />
+          </div>
+        
+          <div>
+            <label  className={styles.label}>Password:</label>
+            <div style={{ position: "relative" }}>
+              <input type={showPassword ? "text" : "password"} value={password} onChange={handlePasswordChange} required className={styles.input} />
+              <div onClick={() => setShowPassword(!showPassword)} 
+              className={styles.VisibilityIcon}
+              >
+                {showPassword ? <VisibilityIcon /> : <VisibilityOffIcon />}
+              </div>
+            </div>
+          </div>
+          <div className={styles.specs}>
+            <h4  className={styles.label}>Sua senha precisa conter:</h4>
+            <span style={{ color: containsSpecialCharacter ? 'green' : 'black' }} className={styles.span}>
+              {containsSpecialCharacter ? <CheckIcon /> : <CloseIcon style={{ color: "#ee0d1f" }} />} 1 caractere especial (\^!@#$%^&*(),.?":{}|)
+            </span>
+            <span style={{ color: containsCapitalLetter ? 'green' : 'black' }} className={styles.span}>
+              {containsCapitalLetter ? <CheckIcon /> : <CloseIcon style={{ color: "#ee0d1f" }} />} 1 letra maiúscula
+            </span>
+            <span style={{ color: containsLowerCase ? 'green' : 'black' }}   className={styles.span}>
+              {containsLowerCase ? <CheckIcon /> : <CloseIcon style={{ color: "#ee0d1f" }} />} 1 letra minúscula
+            </span>
+            <span style={{ color: containsNumber ? 'green' : 'black' }} className={styles.span}>
+              {containsNumber ? <CheckIcon /> : <CloseIcon style={{ color: "#ee0d1f" }} />} 1 número
+            </span>
+          </div>
+          <button type="submit" style={{ backgroundColor: isAddButtonDisabled ? "#ccc" : "#14337C" }} disabled={isAddButtonDisabled} className={styles.formContainer__button}>
+            Cadastrar
+          </button>
+        </form>
       </div>
-    </div>
+    </>
   );
-};
+}
 
-export default ResetPasswordPage;
+export default RegisterUser;
