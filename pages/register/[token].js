9961350f8587d122db51808f8a285e9c74a0fd7e
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2024-09-28 16:21:52 -0300

    feat: migrando pro nextjs... dia 4

--- pages/register/[token].js
+++ pages/register/[token].js
@@ -1,8 +1,7 @@
-"use client"
+"use client";
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useRouter } from 'next/router'; // Import useRouter for Next.js routing
-
 
 import style from "./ResetPasswordPage.module.css";
 import { ToastContainer, toast } from "react-toastify";
@@ -17,47 +16,49 @@ const ResetPasswordPage = () => {
   const [newPassword, setNewPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
   const [message, setMessage] = useState('');
+
   useEffect(() => {
-    // Verifica se o token está presente na URL
+    // Verifies if the token is present in the URL
     if (!token) {
       setMessage('Token inválido.');
     }
   }, [token]);
 
   const handleResetPassword = async () => {
-    try {alert
-      // Verificar se as senhas coincidem
+    if (!token) {
+      setMessage('Token não encontrado.');
+      return;
+    }
+
+    try {
+      // Verify if passwords match
       if (newPassword !== confirmPassword) {
         setMessage('As senhas não coincidem.');
         return;
       }
 
-      // Enviar a solicitação para redefinir a senha
-      const response = await axios.post(`https://serveradmin-whhj.onrender.com/reset-password/:token`, {
-        token,
+      // Send request to reset password
+      const response = await axios.post(`https://serveradmin-whhj.onrender.com/reset-password/${token}`, {
+        token, // use token from the URL
         newPassword,
         confirmPassword,
       });
 
-      // Exibir a mensagem de sucesso
-      setMessage(response.data.error);
-      toast.success("Senha atualisada com sucesso!");
+      // Display success message
+      setMessage(response.data.error ? response.data.error : 'Senha atualizada com sucesso!');
+      toast.success("Senha atualizada com sucesso!");
 
     } catch (error) {
-      // Exibir mensagens de erro
-      setMessage(error.response.data.message);
+      // Display error messages
+      setMessage(error.response?.data?.message || 'Ocorreu um erro ao redefinir a senha.');
     }
   };
 
   return (
     <div>
       <Header />
-      {/* <Helmet>
-        <title>Mudar Senha - Loja Mediewal</title>
-        <meta name="description" content="Veja as últimas novidades em nossa loja, com uma seleção de produtos novos." />
-      </Helmet> */}
       <div className={style.formContainer}>
-      <ToastContainer
+        <ToastContainer
           position="top-center"
           autoClose={5000}
           hideProgressBar={false}
@@ -70,32 +71,32 @@ const ResetPasswordPage = () => {
           theme="light"
           style={{ marginTop: "8rem" }}
         />
-      <h2  className={style.formContainer__h2}>Redefinir Senha</h2>
-      <div className={style.flex}>
-        <span  className={style.formContainer__span}>      {message  && <p>{message}</p>}
-        </span>
-        <label           className={style.formContainer__label}
-        >Nova Senha:</label>
-        <input
-          type="password"
-          value={newPassword}
-          onChange={(e) => setNewPassword(e.target.value)}
-          className={style.formContainer__input}
-        />
-      </div>
-      <div className={style.flex}>
-        <label  className={style.formContainer__label}>Confirmar Senha:</label>
-        <input
-          type="password"
-          value={confirmPassword}
-          onChange={(e) => setConfirmPassword(e.target.value)}
-          className={style.formContainer__input}
-
-        />
-      </div>
-      <button onClick={handleResetPassword}           className={style.formContainer__button}
-      >Redefinir Senha</button>
-
+        <h2 className={style.formContainer__h2}>Redefinir Senha</h2>
+        <div className={style.flex}>
+          {message && <p>{message}</p>}
+          <label className={style.formContainer__label}>Nova Senha:</label>
+          <input
+            type="password"
+            value={newPassword}
+            onChange={(e) => setNewPassword(e.target.value)}
+            className={style.formContainer__input}
+          />
+        </div>
+        <div className={style.flex}>
+          <label className={style.formContainer__label}>Confirmar Senha:</label>
+          <input
+            type="password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            className={style.formContainer__input}
+          />
+        </div>
+        <button
+          onClick={handleResetPassword}
+          className={style.formContainer__button}
+        >
+          Redefinir Senha
+        </button>
       </div>
     </div>
   );
