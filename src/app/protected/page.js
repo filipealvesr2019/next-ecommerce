86a0277c197c118293b5e7ad2d21630d22795291
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2024-10-22 11:45:03 -0300

    fix: corrigi erro de rota

---
+++ src/app/protected/page.js
@@ -0,0 +1,18 @@
+"use client"
+
+import Protected from "@/components/Protected/Protected";
+
+
+
+const page = () => {
+ 
+
+  return (
+    <div>
+    
+      <Protected />
+    </div>
+  );
+}
+
+export default page
